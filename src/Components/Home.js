63f3e7ec156fb37bad14e36e6923dde51d4557c6
gowthamanssel711 <gowthamanssel711@gmail.com>
2022-07-02 20:34:35 +0530

    init

---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+
+function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+
+                <p>
+                  box top
+                </p>
+
+            </header>
+        </div>
+    );
+}
+
+
+export default Home;
