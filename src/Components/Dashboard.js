9d5fefb0ef8733f326a0b1ad1bd46b85bd3a85b2
gowthaman sel <gowthamanssel711@gmail.com>
2022-08-27 16:13:24 +0530

    update

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import   React ,{useEffect,useState} from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
@@ -19,7 +19,7 @@ import ListItemText from '@mui/material/ListItemText';
 import Select from '@mui/material/Select';
 import Checkbox from '@mui/material/Checkbox';
 
-import { Get_Count } from './API/Api';
+import { Get_Maindata,Get_ProductList,Get_InstantCount } from './API/Api';
 
 
 const ITEM_HEIGHT = 48;
@@ -33,9 +33,7 @@ const MenuProps = {
   },
 };
 
-const names = [
- 'A','B','C','D','E','F'
-];
+
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -46,9 +44,87 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function Dashboard() {
-  const [datevalue, setDatevalue] = React.useState(new Date());
 
-  const [personName, setPersonName] = React.useState([]);
+  const [datevalue, setDatevalue] = useState(new Date());
+  const [personName,setPersonName] = useState([]);
+  const [productlist,setProductlist] = useState(['A/UNIT 1 ','B/UNIT 2','C','D','E']);
+  const [product,setProduct]  = useState([]);
+  const [shift,setShift] = useState([]);
+   const [actualop,setActualop] = useState('20');
+
+
+
+
+  useEffect (  ()=> {
+
+const interval = setInterval (() => {
+   get_instant_count();
+
+},5000);
+
+return () => clearInterval(interval);
+
+
+
+
+  },[])
+
+
+
+  useEffect (  ()=> {
+ get_productlist();
+ get_main_data();
+
+
+  },[])
+
+
+const get_main_data = async () => {
+
+  var param = {
+    'date': datevalue,
+    'product': product,
+    'shift':shift
+  }
+
+console.log(param)
+  const data = await Get_Maindata('/get_main_data/',param);
+
+  console.log(data);
+
+}
+
+
+const get_productlist = async () => {
+
+  const data = await Get_ProductList('/get_product_list/','');
+
+  //setProductlist(data.data)
+
+
+}
+
+
+
+const get_instant_count = async () => {
+
+  var param = {
+    'date': datevalue,
+    'product': product,
+    'shift':shift
+  }
+
+
+  const data = await Get_InstantCount('/get_instant_count/',param);
+
+  console.log(`instant count value = ${data.data}`);
+
+  //setActualop(data.data);
+
+  console.log(actualop)
+
+}
+
 
   const handleDeviceChange = (event) => {
     const {
@@ -61,111 +137,178 @@ export default function Dashboard() {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={3}>
-        <Grid item xs>
-        
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
         &nbsp;
-        
-  <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <MobileDatePicker
-              label="DATE"
-              id="outlined-start-adornment"
-              value={datevalue}
-              onChange={(newValue) => {
-                Get_Count("/get_date/",newValue)
-                setDatevalue(newValue);
-              }}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </LocalizationProvider>
-          
-          &nbsp;
-          
-           <TextField
-            label="SHIFT"
-            id="outlined-start-adornment"
-  
-           
-          />
-          
-           &nbsp;
-          
-           <TextField
-            label="SET TIME"
-            id="outlined-start-adornment"
-          />
-          
-          &nbsp;
-          
-              <TextField
-            label="TARGET OP"
-            id="outlined-start-adornment"
-          />
-         
+        <Grid container spacing={3}>
+          <Grid item xs>
 
-        </Grid>
-
-
-        <Grid item xs>
-      &nbsp;
-
-    <FormControl sx={{ width: 190 }}>
-        <InputLabel id="demo-multiple-checkbox-label">Pro</InputLabel>
-        <Select
-          labelId="demo-multiple-checkbox-label"
-          id="demo-multiple-checkbox"
-          multiple
-          value={personName}
-          onChange={handleDeviceChange}
-          input={<OutlinedInput label="Tag" />}
-          renderValue={(selected) => selected.join(', ')}
-          MenuProps={MenuProps}
-        >
-          {names.map((name) => (
-            <MenuItem key={name} value={name}>
-              <Checkbox checked={personName.indexOf(name) > -1} />
-              <ListItemText primary={name} />
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-          
-                   &nbsp;
-   <TextField
-            label="Efficiency"
-            id="outlined-start-adornment"
-  
-          />
-          
-           &nbsp;
-          
-           <TextField
-            label="ACTUAL"
-            id="outlined-start-adornment"
-  
-           
-          />
-          
             &nbsp;
-          
-              <TextField
-            label="ACTUAL OP"
-            id="outlined-start-adornment"
-          />
+
+		 <FormControl sx={{ width: 275 }}>
+            <LocalizationProvider dateAdapter={AdapterDayjs} >
+              <MobileDatePicker
+                label="DATE"
+                sx={{ m: 1, width: '30ch' }}
+                id="outlined-start-adornment"
+                value={datevalue}
+                onChange={(newValue) => {
+                  setDatevalue(newValue);
+                  get_main_data();
+
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+             </FormControl>
+
+            &nbsp;
+
+
+          </Grid>
+
+
+          <Grid item xs>
+            &nbsp;
+
+            <FormControl sx={{ width: 290 }}>
+              <InputLabel id="demo-multiple-checkbox-label">PRODUCT/UNIT</InputLabel>
+              <Select
+                labelId="demo-multiple-checkbox-label"
+                id="demo-multiple-checkbox"
+                multiple
+                value={personName}
+                sx={{ m: 1, width: '30ch' }}
+                onChange={handleDeviceChange}
+                input={<OutlinedInput label="Tag" />}
+                renderValue={(selected) => selected.join(', ')}
+                MenuProps={MenuProps}
+              >
+                {productlist.map((name) => (
+                  <MenuItem key={name} value={name}>
+                    <Checkbox checked={personName.indexOf(name) > -1} />
+                    <ListItemText primary={name} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+          </Grid>
 
         </Grid>
 
-        <Grid item xs={8}>
 
-          <OverAllGraph />
+      </Box>
+
+
+      <Box sx={{ flexGrow: 1 }}>
+        &nbsp;
+        <Grid container spacing={3}>
+          <Grid item xs>
+            &nbsp;
+
+
+            <TextField
+              label="SET TIME "
+              id="outlined-start-adornment"
+              sx={{ m: 1, width: '30ch' }}
+               InputProps={{
+            
+            startAdornment: <InputAdornment position="start">seconds</InputAdornment>,
+          }}
+
+            />
+            &nbsp;
+
+
+          </Grid>
+
+
+          <Grid item xs>
+            &nbsp;
+
+     <TextField
+              label="ACTUAL TIME "
+              id="outlined-start-adornment"
+              sx={{ m: 1, width: '30ch' }}
+                 InputProps={{
+            
+            startAdornment: <InputAdornment position="start">seconds</InputAdornment>,
+          }}	
+
+            />
+       
+
+            &nbsp;
+          </Grid>
 
         </Grid>
 
-   
-      </Grid>
+
+      </Box>
+
+      {/* /////////////////////////////////////////////// */}
+
+      <Box sx={{ flexGrow: 1 }}>
+        &nbsp;
+        <Grid container spacing={3}>
+          <Grid item xs>
+
+            &nbsp;
+
+            <TextField
+              label="TARGET OUTPUT/DAY"
+              id="outlined-start-adornment"
+              sx={{ m: 1, width: '30ch' }}
+
+            />
+
+            &nbsp;
+
+
+          </Grid>
+
+
+          <Grid item xs>
+
+            &nbsp;
+
+           <TextField
+          label="CURRENT OUTPUT"
+          id="outlined-start-adornment"
+          value = {actualop}
+          sx={{ m: 1, width: '30ch' }}
+          InputProps={{
+             readOnly: true,
+            startAdornment: <InputAdornment position="start">cu</InputAdornment>,
+          }}
+        />
+
+            &nbsp;
+
+          </Grid>
+
+        </Grid>
+
+
+      </Box>
+
       &nbsp;
-      <HoursTable/>
-    </Box>
+
+      <TextField
+        label="EFFICIENCY"
+        id="outlined-start-adornment"
+	
+        sx={{ m: 1, width: '30ch' }}
+          inputProps={{min: 0, style: { textAlign: 'center' }}}
+       
+      />
+
+       
+
+      &nbsp;
+
+      <HoursTable />
+    </div>
   );
 }
